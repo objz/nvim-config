674550d867573b719bbf01f483cc9c28bf224284
objz <me@objz.dev>
2026-02-02 21:38:06 +0100

    feat: uses now floatterm instead of toggleterm

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,35 +1,47 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    cmd = { "ToggleTerm", "TermExec" },
-    keys = {
-      { "<F6>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = { "n", "t" } },
-      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Floating Terminal" },
-      { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle Horizontal Terminal" },
-      { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle Vertical Terminal" },
-    },
-    opts = {
-      size = 20,
-      open_mapping = [[<c-\>]],
-      hide_numbers = false,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
-      direction = "float",
-      close_on_exit = true,
-      shell = vim.o.shell,
-      float_opts = {
-        border = "curved",
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
+    {
+        --   "akinsho/toggleterm.nvim",
+        --   version = "*",
+        --   cmd = { "ToggleTerm", "TermExec" },
+        --   keys = {
+        --     { "<F6>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal", mode = { "n", "t" } },
+        --     { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Floating Terminal" },
+        --     { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle Horizontal Terminal" },
+        --     { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle Vertical Terminal" },
+        --   },
+        --   opts = {
+        --     size = 20,
+        --     open_mapping = [[<c-\>]],
+        --     hide_numbers = false,
+        --     shade_filetypes = {},
+        --     shade_terminals = true,
+        --     shading_factor = 2,
+        --     start_in_insert = true,
+        --     insert_mappings = true,
+        --     persist_size = true,
+        --     direction = "float",
+        --     close_on_exit = true,
+        --     shell = vim.o.shell,
+        --     float_opts = {
+        --       border = "curved",
+        --       winblend = 0,
+        --       highlights = {
+        --         border = "Normal",
+        --         background = "Normal",
+        --       },
+        --     },
+        --   },
+        -- },
+
+
+        "nvzone/floaterm",
+        dependencies = "nvzone/volt",
+        opts = {},
+        cmd = "FloatermToggle",
+        keys = {
+            { "<F6>", "<cmd>FloatermToggle<cr>", desc = "Toggle Terminal", mode = { "n", "t" } },
         },
-      },
-    },
-  },
+
+    }
+
 }

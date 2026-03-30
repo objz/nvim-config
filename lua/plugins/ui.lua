064f4ed522d19cb5c911602a21273f396543aca0
objz <me@objz.dev>
2026-03-30 17:10:40 +0200

    nui fix

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -240,7 +240,12 @@ return {
         },
         dependencies = {
             "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
+            {
+                "rcarriga/nvim-notify",
+                opts = {
+                    background_colour = "#000000",
+                },
+            },
         },
     },
 
